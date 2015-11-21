35563b04fe9dbfea7b030fa6828ceb55eb709549
fish5421 <petercorreia2013@gmail.com>
2015-11-21 15:14:17 -0500

    form test 2

--- form.php
+++ form.php
@@ -4,7 +4,7 @@ define('DB_USER', 'root');
 define('DB_PASSWORD', 'Ginger422737');
 define('DB_HOST', 'localhost');
 
-$link = mysql_connect(DB_HOST, DB_USER,DB_PASSWORD);
+$link = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD);
 
 if (!$link) {
 	die('Could not connect: ' .mysql_error());
@@ -23,6 +23,23 @@ $sql = "INSERT INTO test1 (Name) VALUES ('$value')";
 if (!mysql_query($sql)) {
 	die('Error: ' . mysql_error());
 }
+
+$value2 = $_POST[email2];
+
+$sql2 = "INSERT INTO test1 (Email) VALUES ('$value2')";
+
+if (!mysql_query($sql2)) {
+	die('Error: ' . mysql_error());
+}
+
+$value3 = $_POST[message2];
+
+$sql3 = "INSERT INTO test1 (Message) VALUES ('$value3')";
+
+if (!mysql_query($sql3)) {
+	die('Error: ' . mysql_error());
+}
+
 
 mysql_close();
 ?>
