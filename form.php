d8d5295b9465581e2154bfc143099f24366819ce
fish5421 <petercorreia2013@gmail.com>
2015-11-21 15:08:51 -0500

    Form #1

--- form.php
+++ form.php
@@ -1,8 +1,28 @@
-<html>
-<body>
+<?php
+define('DB_NAME', 'fcc_form');
+define('DB_USER', 'root');
+define('DB_PASSWORD', 'Ginger422737');
+define('DB_HOST', 'localhost');
 
-Welcome <?php echo $_POST["name"]; ?><br>
-Your email address is: <?php echo $_POST["email"]; ?>
+$link = mysql_connect(DB_HOST, DB_USER,DB_PASSWORD);
 
-</body>
-</html>
+if (!$link) {
+	die('Could not connect: ' .mysql_error());
+}
+
+$db_selected = mysql_select_db(DB_NAME, $link);
+
+if (!$db_selected) {
+	die('Can\'t use' .DB_NAME . ':' .mysql_error());
+}
+
+$value = $_POST[name2];
+
+$sql = "INSERT INTO test1 (Name) VALUES ('$value')";
+
+if (!mysql_query($sql)) {
+	die('Error: ' . mysql_error());
+}
+
+mysql_close();
+?>
