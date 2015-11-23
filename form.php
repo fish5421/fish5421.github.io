134a66a6a048aa54ad6695fbd36e49e88c7ec05f
fish5421 <petercorreia2013@gmail.com>
2015-11-23 18:26:03 -0500

    test again

--- form.php
+++ form.php
@@ -12,33 +12,33 @@ if (!$link) {
 
 $db_selected = mysql_select_db(DB_NAME, $link);
 
-if (!$db_selected) {
-	die('Can\'t use' .DB_NAME . ':' .mysql_error());
-}
+// if (!$db_selected) {
+// 	die('Can\'t use' .DB_NAME . ':' .mysql_error());
+// }
 
-$value = $_POST[name2];
+// $value = $_POST[name2];
 
-$sql = "INSERT INTO test1 (Name) VALUES ('$value')";
+// $sql = "INSERT INTO test1 (Name) VALUES ('$value')";
 
-if (!mysql_query($sql)) {
-	die('Error: ' . mysql_error());
-}
+// if (!mysql_query($sql)) {
+// 	die('Error: ' . mysql_error());
+// }
 
-$value2 = $_POST[email2];
+// $value2 = $_POST[email2];
 
-$sql2 = "INSERT INTO test1 (Email) VALUES ('$value2')";
+// $sql2 = "INSERT INTO test1 (Email) VALUES ('$value2')";
 
-if (!mysql_query($sql2)) {
-	die('Error: ' . mysql_error());
-}
+// if (!mysql_query($sql2)) {
+// 	die('Error: ' . mysql_error());
+// }
 
-$value3 = $_POST[message2];
+// $value3 = $_POST[message2];
 
-$sql3 = "INSERT INTO test1 (Message) VALUES ('$value3')";
+// $sql3 = "INSERT INTO test1 (Message) VALUES ('$value3')";
 
-if (!mysql_query($sql3)) {
-	die('Error: ' . mysql_error());
-}
+// if (!mysql_query($sql3)) {
+// 	die('Error: ' . mysql_error());
+// }
 
 
 mysql_close();
