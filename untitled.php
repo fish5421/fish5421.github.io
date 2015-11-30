17f215598891ad4c1761faddfe49cf709cc1f7cc
fish5421 <petercorreia2013@gmail.com>
2015-11-29 22:39:43 -0500

    changes

---
+++ untitled.php
@@ -0,0 +1,25 @@
+<?php
+
+$servername = "192.168.1.142:8889";
+$username = "peter";
+$password = "Ginger422737";
+$dbname = "myDB";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "INSERT INTO MyGuests (firstname, lastname, email)
+VALUES ('Emily', 'Doe', 'john@example.com')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
